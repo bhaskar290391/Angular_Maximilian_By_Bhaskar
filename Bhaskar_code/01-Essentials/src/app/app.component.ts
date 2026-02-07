505b9c12656cd83693a4733b1341c11c02024274
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-02-07 17:45:18 +0530

    creating task component and add input decorator

--- Bhaskar_code/01-Essentials/src/app/app.component.ts
+++ Bhaskar_code/01-Essentials/src/app/app.component.ts
@@ -2,18 +2,24 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { UserComponent } from './components/user/user.component';
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './components/tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  selectedUserId = 'u1';
 
   onSelectUser(id: any) {
-    console.log('Selected user id is ' + id);
+    this.selectedUserId = id;
+  }
+
+  get selectUser() {
+    return this.users.find((user) => user.id == this.selectedUserId)!;
   }
 }
