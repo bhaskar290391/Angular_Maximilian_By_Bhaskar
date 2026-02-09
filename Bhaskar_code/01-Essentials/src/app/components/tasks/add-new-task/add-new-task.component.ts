995582807c8185f6982c68983d5efdc65d3e4877
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-02-09 18:09:24 +0530

    Form input using NG Model

--- Bhaskar_code/01-Essentials/src/app/components/tasks/add-new-task/add-new-task.component.ts
+++ Bhaskar_code/01-Essentials/src/app/components/tasks/add-new-task/add-new-task.component.ts
@@ -1,14 +1,25 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-new-task.component.html',
   styleUrl: './add-new-task.component.css',
 })
 export class AddNewTaskComponent {
   @Output() cancelled = new EventEmitter();
+
+  //using Signal
+  // enteredTitle = signal('');
+  // enteredSummary = signal('');
+  // enteredDate = signal('');
+
+  //traditional way of declaring ngmodel variable
+  enteredTitle: string = '';
+  enteredSummary: string = '';
+  enteredDate: string = '';
 
   onCancelled() {
     this.cancelled.emit();
