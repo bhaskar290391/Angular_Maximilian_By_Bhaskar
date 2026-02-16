48bbf2dc261dc39dbb46bb2e49ea5e27e15723af
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-02-16 19:58:29 +0530

    adding template variable

--- Bhaskar_code/05-component-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ Bhaskar_code/05-component-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
-  imports: [ControlComponent, ButtonComponent],
+  imports: [ControlComponent, ButtonComponent, FormsModule],
 })
-export class NewTicketComponent {}
+export class NewTicketComponent {
+  OnSubmit(input: string, texterea: string) {
+    console.log('Clicked !!!' + input + ' ==> ' + texterea);
+  }
+}
