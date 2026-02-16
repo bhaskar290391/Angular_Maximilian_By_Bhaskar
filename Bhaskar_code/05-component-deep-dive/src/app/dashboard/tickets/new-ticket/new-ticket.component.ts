34dc1180a8608458f10a913571bd81c733f43c18
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-02-16 16:59:03 +0530

    csss issue resolved

--- Bhaskar_code/05-component-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ Bhaskar_code/05-component-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -7,6 +7,6 @@ import { ControlComponent } from '../../../shared/control/control.component';
   standalone: true,
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
-  imports: [ControlComponent],
+  imports: [ControlComponent, ButtonComponent],
 })
 export class NewTicketComponent {}
