466d5429190851607f93c3c516c635fb7ff6ed7d
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-02-19 22:16:27 +0530

    different way of  injecting task service

--- Bhaskar_code/09-service-dependency-injection-deepdive/src/main.ts
+++ Bhaskar_code/09-service-dependency-injection-deepdive/src/main.ts
@@ -1,5 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
+import { TaskService } from './app/tasks/taskservice.service';
+
+// bootstrapApplication(AppComponent, { providers: [TaskService] }).catch((err) =>
+//   console.error(err),
+// );
 
 bootstrapApplication(AppComponent).catch((err) => console.error(err));
