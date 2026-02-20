87bff0fb5371458ead21e67fdc6086762d1d3302
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-02-20 10:49:10 +0530

    adding service into another service

--- Bhaskar_code/09-service-dependency-injection-deepdive/src/app/tasks/taskservice.service.ts
+++ Bhaskar_code/09-service-dependency-injection-deepdive/src/app/tasks/taskservice.service.ts
@@ -1,10 +1,13 @@
-import { Injectable, signal } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { Task, TaskStatus } from './task.model';
+import { LoggingService } from '../logging.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TaskService {
+  private loggingService = inject(LoggingService);
+
   constructor() {}
 
   private tasks = signal<Task[]>([]);
@@ -19,6 +22,8 @@ export class TaskService {
     };
 
     this.tasks.update((oldTask) => [...oldTask, newtask]);
+
+    this.loggingService.log('Adding new Task to the list');
   }
 
   updateTaskService(taskid: string, status: TaskStatus) {
@@ -28,5 +33,6 @@ export class TaskService {
         task.id === taskid ? { ...task, status: status } : task,
       ),
     );
+    this.loggingService.log('updating status of task to ' + status);
   }
 }
