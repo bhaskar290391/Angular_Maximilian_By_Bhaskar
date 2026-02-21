a68346f45ed4bbb1cad6f9c513803f6cb6127cf6
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-02-21 11:35:02 +0530

    custom directive enabled for onlclick event

--- Bhaskar_code/07-directives-deep-dive/src/app/safe-link.directive.ts
+++ Bhaskar_code/07-directives-deep-dive/src/app/safe-link.directive.ts
@@ -3,9 +3,22 @@ import { Directive } from '@angular/core';
 @Directive({
   selector: 'a[appSafeLink]',
   standalone: true,
+  host: {
+    '(click)': 'onConfirmLeavePage($event)',
+  },
 })
 export class SafeLinkDirective {
   constructor() {
     console.log('Directive initiated ');
+  }
+
+  onConfirmLeavePage(event: MouseEvent) {
+    const leavePage = window.confirm('Do You want to leave this page ??');
+
+    if (leavePage) {
+      return;
+    }
+
+    event.preventDefault();
   }
 }
