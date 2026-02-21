7c25e52a0cb32c58f99dfce87cdb3592b4222433
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-02-21 11:55:41 +0530

    custom directive with input data

--- Bhaskar_code/07-directives-deep-dive/src/app/safe-link.directive.ts
+++ Bhaskar_code/07-directives-deep-dive/src/app/safe-link.directive.ts
@@ -1,4 +1,4 @@
-import { Directive } from '@angular/core';
+import { Directive, Input } from '@angular/core';
 
 @Directive({
   selector: 'a[appSafeLink]',
@@ -8,13 +8,18 @@ import { Directive } from '@angular/core';
   },
 })
 export class SafeLinkDirective {
+  @Input() queryParam?: string;
+
   constructor() {
     console.log('Directive initiated ');
   }
 
   onConfirmLeavePage(event: MouseEvent) {
     const leavePage = window.confirm('Do You want to leave this page ??');
+    const address = (event.target as HTMLAnchorElement).href;
 
+    (event.target as HTMLAnchorElement).href =
+      address + '?from=' + this.queryParam;
     if (leavePage) {
       return;
     }
