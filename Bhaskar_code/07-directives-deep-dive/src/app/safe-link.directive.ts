8daad69d909dc262cdc8fa38f45d8520daa21169
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-02-21 12:14:27 +0530

    using host elemet ref

--- Bhaskar_code/07-directives-deep-dive/src/app/safe-link.directive.ts
+++ Bhaskar_code/07-directives-deep-dive/src/app/safe-link.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Input } from '@angular/core';
+import { Directive, ElementRef, inject, Input } from '@angular/core';
 
 @Directive({
   selector: 'a[appSafeLink]',
@@ -10,17 +10,24 @@ import { Directive, Input } from '@angular/core';
 export class SafeLinkDirective {
   @Input() queryParam?: string;
 
+  private hostElementRef = inject<ElementRef<HTMLAnchorElement>>(ElementRef);
+
   constructor() {
     console.log('Directive initiated ');
   }
 
   onConfirmLeavePage(event: MouseEvent) {
     const leavePage = window.confirm('Do You want to leave this page ??');
-    const address = (event.target as HTMLAnchorElement).href;
+    // const address = (event.target as HTMLAnchorElement).href;
 
-    (event.target as HTMLAnchorElement).href =
-      address + '?from=' + this.queryParam;
+    // (event.target as HTMLAnchorElement).href =
+    //   address + '?from=' + this.queryParam;
+
     if (leavePage) {
+      const address = this.hostElementRef.nativeElement.href;
+
+      this.hostElementRef.nativeElement.href =
+        address + '?from=' + this.queryParam;
       return;
     }
 
